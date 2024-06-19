0a9caab34e160aca22e830ee7480b3da833d89b3
Timeless0911 <1604889533@qq.com>
2024-06-19 19:54:11 +0800

    feat: basic load config and build architecture

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,11 @@
 export { build } from './build';
 export { runCli } from './cli/commands';
 export { prepareCli } from './cli/prepare';
+export { defineConfig } from './config';
 
 export * from './utils/logger';
 export * from './utils/helper';
 
-export type { RslibConfig } from './types';
+export * from './types/config';
 
 export const version = RSLIB_VERSION;
---
+++ packages/core/src/types/config/index.ts
@@ -0,0 +1,24 @@
+import type { RsbuildConfig } from '@rsbuild/core';
+
+export type Format = 'esm' | 'cjs' | 'umd' | 'iife';
+
+export interface LibConfig extends RsbuildConfig {
+  format?: Format;
+}
+
+export interface RslibConfig extends RsbuildConfig {
+  lib?: LibConfig[];
+}
+
+export type ConfigParams = {
+  env: string;
+  command: string;
+  envMode?: string;
+};
+
+export type RslibConfigSyncFn = (env: ConfigParams) => RslibConfig;
+export type RslibConfigAsyncFn = (env: ConfigParams) => Promise<RslibConfig>;
+export type RslibConfigExport =
+  | RslibConfig
+  | RslibConfigSyncFn
+  | RslibConfigAsyncFn;
