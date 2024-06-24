a2769f92f2c74150a1aeac4b34f865dfeec0db4d
fi3ework <fi3ework@gmail.com>
2024-06-24 22:25:26 +0800

    chore: bump @rspack/core

--- e2e/cases/define/js/src/value.js
+++
@@ -1,4 +0,0 @@
-// TODO: Rspack only can handle exports from concatenated modules now.
-// Adding this file to let the compile pass as the moment.
-// Remove this file when Rspack could export from single module.`
-export const a = 1;
