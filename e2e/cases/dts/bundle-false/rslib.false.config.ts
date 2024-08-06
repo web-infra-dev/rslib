e32c05accccc3559497f1302ba803139ff4be724
Timeless0911 <1604889533@qq.com>
2024-08-06 16:44:10 +0800

    test: add tests of dts: false and dts.distPath

---
+++ e2e/cases/dts/bundle-false/rslib.false.config.ts
@@ -0,0 +1,19 @@
+import { generateBundleCjsConfig, generateBundleEsmConfig } from '@e2e/helper';
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  lib: [
+    generateBundleEsmConfig(__dirname, {
+      bundle: false,
+      dts: false,
+    }),
+    generateBundleCjsConfig(__dirname, {
+      bundle: false,
+    }),
+  ],
+  source: {
+    entry: {
+      main: ['./src/**'],
+    },
+  },
+});
--- e2e/cases/dts/index.test.ts
+++ e2e/cases/dts/index.test.ts
@@ -1,20 +1,76 @@
 import { join } from 'node:path';
 import { buildAndGetResults } from '@e2e/helper';
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
-test('dts when bundle: false', async () => {
-  const fixturePath = join(__dirname, 'bundle-false');
-  const { files, contents } = await buildAndGetResults(fixturePath, 'dts');
+describe('dts when bundle: false', () => {
+  test('basic - bundleless dts', async () => {
+    const fixturePath = join(__dirname, 'bundle-false');
+    const { files, contents } = await buildAndGetResults(
+      fixturePath,
+      'rslib.config.ts',
+      'dts',
+    );
 
-  expect(files.esm?.length).toBe(4);
-  expect(files.esm?.[0]!.endsWith('.d.ts')).toEqual(true);
-  expect(contents.esm).toMatchSnapshot();
+    expect(files.esm?.length).toBe(4);
+    expect(files.esm?.[0]!.endsWith('.d.ts')).toEqual(true);
+    expect(contents.esm).toMatchSnapshot();
+  });
+
+  test('dts false - bundleless dts', async () => {
+    const fixturePath = join(__dirname, 'bundle-false');
+    const { files } = await buildAndGetResults(
+      fixturePath,
+      'rslib.false.config.ts',
+      'dts',
+    );
+
+    expect(files.esm).toBe(undefined);
+  });
+
+  test('distPath - bundleless dts', async () => {
+    const fixturePath = join(__dirname, 'bundle-false');
+    const { files } = await buildAndGetResults(
+      fixturePath,
+      'rslib.distpath.config.ts',
+      'dts',
+    );
+    expect(files.esm?.length).toBe(4);
+    expect(files.esm?.[0]!.startsWith('./dist/custom')).toEqual(true);
+  });
 });
 
-test('dts when bundle: true', async () => {
-  const fixturePath = join(__dirname, 'bundle');
-  const { entryFiles, entries } = await buildAndGetResults(fixturePath, 'dts');
+describe('dts when bundle: true', () => {
+  test('basic - bundle dts', async () => {
+    const fixturePath = join(__dirname, 'bundle');
+    const { entryFiles, entries } = await buildAndGetResults(
+      fixturePath,
+      'rslib.config.ts',
+      'dts',
+    );
 
-  expect(entryFiles.esm!.endsWith('index.d.ts')).toEqual(true);
-  expect(entries).toMatchSnapshot();
+    expect(entryFiles.esm!.endsWith('index.d.ts')).toEqual(true);
+    expect(entries).toMatchSnapshot();
+  });
+
+  test('dts false - bundle dts', async () => {
+    const fixturePath = join(__dirname, 'bundle');
+    const { entryFiles } = await buildAndGetResults(
+      fixturePath,
+      'rslib.false.config.ts',
+      'dts',
+    );
+
+    expect(entryFiles.esm).toEqual(undefined);
+  });
+
+  test('distPath - bundle dts', async () => {
+    const fixturePath = join(__dirname, 'bundle');
+    const { entryFiles } = await buildAndGetResults(
+      fixturePath,
+      'rslib.distpath.config.ts',
+      'dts',
+    );
+
+    expect(entryFiles.esm!.startsWith('./dist/custom')).toEqual(true);
+  });
 });
