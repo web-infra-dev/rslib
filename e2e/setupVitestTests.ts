38fcca542003577bde580e34406998efe93626da
Soon <sooniter@gmail.com>
2024-08-09 19:41:48 +0800

    ci: add windows test (#61)

--- e2e/setupVitestTests.ts
+++ e2e/setupVitestTests.ts
@@ -1,8 +1,16 @@
+import path from 'node:path';
 import process from 'node:process';
-import { beforeEach } from 'vitest';
+import { createSnapshotSerializer } from 'path-serializer';
+import { beforeEach, expect } from 'vitest';
 
 beforeEach(() => {
   // since our NODE_ENV injection logic is via cli, we need to
   // delete "test" NODE_ENV to avoid affecting the default build config
   delete process.env.NODE_ENV;
 });
+
+expect.addSnapshotSerializer(
+  createSnapshotSerializer({
+    workspace: path.join(__dirname, '..'),
+  }),
+);
--- packages/core/tests/lcp.test.ts
+++ packages/core/tests/lcp.test.ts
@@ -1,3 +1,4 @@
+import os from 'node:os';
 import { vol } from 'memfs';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { calcLongestCommonPath } from '../src/utils/helper';
@@ -19,35 +20,57 @@ describe('LCP calculate correctly', () => {
   it('correct 1', async () => {
     vol.fromJSON({ '/Users/Someone/project-a/src': null });
 
-    const result = await calcLongestCommonPath([
-      '/Users/Someone/project-a/src/helpers',
-      '/Users/Someone/project-a/src',
-      '/Users/Someone/project-a/src/utils',
-    ]);
-    expect(result).toBe('/Users/Someone/project-a/src');
+    if (os.platform() !== 'win32') {
+      const result = await calcLongestCommonPath([
+        '/Users/Someone/project-a/src/helpers',
+        '/Users/Someone/project-a/src',
+        '/Users/Someone/project-a/src/utils',
+      ]);
+      expect(result).toEqual('/Users/Someone/project-a/src');
+    } else {
+      const result = await calcLongestCommonPath([
+        'D:\\Users\\Someone\\project-a\\src\\helpers',
+        'D:\\Users\\Someone\\project-a\\src',
+        'D:\\Users\\Someone\\project-a\\src\\utils',
+      ]);
+      expect(result).toEqual('D:\\Users\\Someone\\project-a\\src');
+    }
   });
 
   it('correct 2', async () => {
     vol.fromJSON({ '/Users/Someone/project-monorepo': null });
 
-    const result = await calcLongestCommonPath([
-      '/Users/Someone/project-monorepo/packages-a/src/index.ts',
-      '/Users/Someone/project-monorepo/packages-util/src/index.js',
-      '/Users/Someone/project-monorepo/script.js',
-    ]);
-
-    expect(result).toBe('/Users/Someone/project-monorepo');
+    if (os.platform() !== 'win32') {
+      const result = await calcLongestCommonPath([
+        '/Users/Someone/project-monorepo/packages-a/src/index.ts',
+        '/Users/Someone/project-monorepo/packages-util/src/index.js',
+        '/Users/Someone/project-monorepo/script.js',
+      ]);
+      expect(result).toEqual('/Users/Someone/project-monorepo');
+    } else {
+      const result = await calcLongestCommonPath([
+        'D:\\Users\\Someone\\project-monorepo\\packages-a\\src\\index.ts',
+        'D:\\Users\\Someone\\project-monorepo\\packages-util\\src\\index.js',
+        'D:\\Users\\Someone\\project-monorepo\\script.js',
+      ]);
+      expect(result).toEqual('D:\\Users\\Someone\\project-monorepo');
+    }
   });
 
   it('correct 3', async () => {
     vol.fromJSON({
       '/Users/Someone/project/src/index.js': '',
     });
-
-    const result = await calcLongestCommonPath([
-      '/Users/Someone/project/src/index.js',
-    ]);
-
-    expect(result).toBe('/Users/Someone/project/src');
+    if (os.platform() !== 'win32') {
+      const result = await calcLongestCommonPath([
+        '/Users/Someone/project/src/index.js',
+      ]);
+      expect(result).toEqual('/Users/Someone/project/src');
+    } else {
+      const result = await calcLongestCommonPath([
+        'D:\\Users\\Someone\\project\\src\\index.js',
+      ]);
+      expect(result).toEqual('D:\\Users\\Someone\\project\\src');
+    }
   });
 });
