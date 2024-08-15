9adf980a3898f63dd590deb93d4af2bf5cd97969
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2024-08-15 17:30:50 +0800

    chore: use Rslib for bootstrapping (#92)

---
+++ packages/core/rslib.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'rslib';
+
+export default defineConfig({
+  lib: [
+    {
+      format: 'esm',
+      output: {
+        syntax: ['node 16'],
+      },
+      dts: {
+        bundle: false,
+        distPath: './dist-types',
+      },
+    },
+  ],
+  source: {
+    define: {
+      RSLIB_VERSION: JSON.stringify(require('./package.json').version),
+    },
+    entry: {
+      main: './src/index.ts',
+    },
+  },
+  output: {
+    target: 'node',
+    externals: {
+      picocolors: '../compiled/picocolors/index.js',
+      'fast-glob': '../compiled/fast-glob/index.js',
+      commander: '../compiled/commander/index.js',
+    },
+  },
+});
