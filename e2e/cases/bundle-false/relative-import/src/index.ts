beb8843d41ffee02f5b9aabb3542c61cfa5c559c
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2024-08-27 15:48:26 +0800

    fix: add extension when relative import already have an extension (#132)

--- e2e/cases/bundle-false/relative-import/src/index.ts
+++ e2e/cases/bundle-false/relative-import/src/index.ts
@@ -1,3 +1,8 @@
 import { bar } from './bar';
+import { baz } from './baz.js';
+// @ts-ignore
+import { foo } from './foo.js';
+// @ts-ignore
+import { qux } from './qux.cjs';
 
-export const foo = 'foo' + bar;
+export const text = foo + bar + baz + qux;
