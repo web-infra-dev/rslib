ba5e65692062a93216a8c0ef4a60d220ef7ff51d
fi3ework <fi3ework@gmail.com>
2024-08-30 01:34:51 +0800

    fix: warn should handles all externals config

--- e2e/cases/externals/module-import-warn/src/index.ts
+++ e2e/cases/externals/module-import-warn/src/index.ts
@@ -1,6 +1,12 @@
-export const baz = () => {
+export const baz = async () => {
   const foo = require('foo');
   const bar = require('bar');
+  const baz = require('./baz'); // not externalized
+  const qux = require('qux');
+  const quxx = await import('quxx');
   foo();
   bar();
+  baz();
+  qux();
+  quxx();
 };
