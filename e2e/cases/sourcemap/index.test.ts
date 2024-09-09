a8e0042541397d5491c5acfa00b01e61c17db384
Timeless0911 <1604889533@qq.com>
2024-09-09 16:35:24 +0800

    chore: update

--- e2e/cases/sourcemap/index.test.ts
+++ e2e/cases/sourcemap/index.test.ts
@@ -1,3 +1,4 @@
+import os from 'node:os';
 import { join } from 'node:path';
 import { buildAndGetResults } from '@e2e/helper';
 import { expect, test } from 'vitest';
@@ -38,12 +39,24 @@ test('should generate js inline sourcemap: inline-cheap-module-source-map', asyn
       "<ROOT>/e2e/cases/sourcemap/inline/dist/esm/index.js",
     ]
   `);
-  expect(code).toMatchInlineSnapshot(`
-    [
-      "const foo = 'foo';
-    export { foo };
 
-    //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zb3VyY2VtYXAtaW5saW5lLXRlc3QvLi4vX19maXh0dXJlc19fL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgZm9vID0gJ2Zvbyc7XG4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEifQ==",
-    ]
-  `);
+  if (os.platform() === 'win32') {
+    expect(code).toMatchInlineSnapshot(`
+      [
+        "const foo = 'foo';
+      export { foo };
+
+      //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zb3VyY2VtYXAtaW5saW5lLXRlc3QvLi4vX19maXh0dXJlc19fL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgZm9vID0gJ2Zvbyc7XHJcbiJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSJ9",
+      ]
+    `);
+  } else {
+    expect(code).toMatchInlineSnapshot(`
+      [
+        "const foo = 'foo';
+      export { foo };
+
+      //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zb3VyY2VtYXAtaW5saW5lLXRlc3QvLi4vX19maXh0dXJlc19fL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgZm9vID0gJ2Zvbyc7XG4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEifQ==",
+      ]
+    `);
+  }
 });
