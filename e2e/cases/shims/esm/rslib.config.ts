600937cebd061459a43d4f3e81bf09c96aee0375
Wei <fi3ework@gmail.com>
2024-09-26 12:11:55 +0800

    fix: do not transform esX for rspack.target field (#237)

--- e2e/cases/shims/esm/rslib.config.ts
+++ e2e/cases/shims/esm/rslib.config.ts
@@ -2,7 +2,23 @@ import { generateBundleEsmConfig } from '@e2e/helper';
 import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
-  lib: [generateBundleEsmConfig()],
+  lib: [
+    generateBundleEsmConfig({
+      output: {
+        distPath: {
+          root: './dist/normal',
+        },
+      },
+    }),
+    generateBundleEsmConfig({
+      syntax: 'esnext',
+      output: {
+        distPath: {
+          root: './dist/with-syntax',
+        },
+      },
+    }),
+  ],
   output: {
     target: 'node',
   },
