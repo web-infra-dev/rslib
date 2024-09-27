1c05552c93c50a6ae6ad07679c2f1670e6b6265b
Soon <sooniter@gmail.com>
2024-09-27 13:46:35 +0800

    test(e2e): add react-component example test (#239)

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   // Playwright test files with `.pw.` to distinguish from Vitest test files
   testMatch: /.*pw.(test|spec).(js|ts|mjs)/,
+  // Retry on CI
+  retries: process.env.CI ? 3 : 0,
+  // Print line for each test being run in CI
+  reporter: 'list',
 });
