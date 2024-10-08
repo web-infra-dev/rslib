2d13902ca8b7f16b88cf3cc7a12f580a969a0896
SoonIter <sooniter@gmail.com>
2024-10-08 21:26:54 +0800

    chore: add resourceQuery

--- packages/core/src/css/cssConfig.ts
+++ packages/core/src/css/cssConfig.ts
@@ -23,6 +23,26 @@ export function isCssFile(filepath: string): boolean {
 }
 
 const CSS_MODULE_REG = /\.module\.\w+$/i;
+
+/**
+ * This function is modified based on
+ * https://github.com/web-infra-dev/rspack/blob/7b80a45a1c58de7bc506dbb107fad6fda37d2a1f/packages/rspack/src/loader-runner/index.ts#L903
+ */
+const PATH_QUERY_FRAGMENT_REGEXP =
+  /^((?:\u200b.|[^?#\u200b])*)(\?(?:\u200b.|[^#\u200b])*)?(#.*)?$/;
+export function parsePathQueryFragment(str: string): {
+  path: string;
+  query: string;
+  fragment: string;
+} {
+  const match = PATH_QUERY_FRAGMENT_REGEXP.exec(str);
+  return {
+    path: match?.[1]?.replace(/\u200b(.)/g, '$1') || '',
+    query: match?.[2] ? match[2].replace(/\u200b(.)/g, '$1') : '',
+    fragment: match?.[3] || '',
+  };
+}
+
 export function isCssModulesFile(
   filepath: string,
   auto: CssLoaderOptionsAuto,
@@ -37,7 +57,9 @@ export function isCssModulesFile(
   }
 
   if (typeof auto === 'function') {
-    return auto(filepath, '', '');
+    const { path, query, fragment } = parsePathQueryFragment(filepath);
+    // this is a mock for loader
+    return auto(path, query, fragment);
   }
 
   return false;
