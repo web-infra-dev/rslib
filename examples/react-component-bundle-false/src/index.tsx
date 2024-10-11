c0f84a4a3c66f3b330faabd8e7e8e1f7cb40eaff
nyqykk <nieyan.kk@bytedance.com>
2024-10-11 16:14:36 +0800

    chore: revert change

---
+++ examples/react-component-bundle-false/src/index.tsx
@@ -0,0 +1,15 @@
+import { CounterButton } from './components/CounterButton/index';
+import { useCounter } from './useCounter';
+import './index.scss';
+
+export const Counter: React.FC = () => {
+  const { count, increment, decrement } = useCounter();
+
+  return (
+    <div>
+      <h2 className="counter-text">Counter: {count}</h2>
+      <CounterButton onClick={decrement} label="-" />
+      <CounterButton onClick={increment} label="+" />
+    </div>
+  );
+};
