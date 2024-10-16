f1055a96f7db20b3b6da8779ddc85289b676caeb
fi3ework <fi3ework@gmail.com>
2024-10-16 23:58:22 +0800

    fix: correct ECMA version browserslist mapping

--- packages/core/tests/syntax.test.ts
+++ packages/core/tests/syntax.test.ts
@@ -1,35 +1,85 @@
 import { describe, expect, test } from 'vitest';
 import {
+  ESX_TO_BROWSERSLIST,
   transformSyntaxToBrowserslist,
   transformSyntaxToRspackTarget,
 } from '../src/utils/syntax';
 
+const compareSemver = (a: string, b: string) => {
+  const [aMajor, aMinor, aPatch] = a.split('.').map(Number);
+  const [bMajor, bMinor, bPatch] = b.split('.').map(Number);
+
+  if (aMajor !== bMajor) {
+    return aMajor - bMajor;
+  }
+  if (aMinor !== bMinor) {
+    return aMinor - bMinor;
+  }
+  return aPatch - bPatch;
+};
+
+describe('ESX_TO_BROWSERSLIST', () => {
+  test('some ECMA version queries should be the same', () => {
+    expect(ESX_TO_BROWSERSLIST.es6).toStrictEqual(ESX_TO_BROWSERSLIST.es2015);
+    expect(ESX_TO_BROWSERSLIST.esnext).toStrictEqual(
+      ESX_TO_BROWSERSLIST.es2024,
+    );
+  });
+
+  test('ECMA version mapped browserslist queries should increments', () => {
+    const sortedVersions = [
+      'es5',
+      'es6',
+      'es2015',
+      'es2016',
+      'es2017',
+      'es2018',
+      'es2019',
+      'es2020',
+      'es2021',
+      'es2022',
+      'es2023',
+      'es2024',
+      'esnext',
+    ];
+
+    for (let i = 1; i < sortedVersions.length; i++) {
+      const prev = sortedVersions[i - 1];
+      const current = sortedVersions[i];
+      for (const query of Object.keys(ESX_TO_BROWSERSLIST[current])) {
+        const prevQuery = ESX_TO_BROWSERSLIST[prev][query];
+        const currQuery = ESX_TO_BROWSERSLIST[current][query];
+        if (prevQuery && currQuery) {
+          expect(compareSemver(currQuery, prevQuery)).toBeGreaterThanOrEqual(0);
+        }
+      }
+    }
+  });
+});
+
 describe('transformSyntaxToBrowserslist', () => {
   test('esX', () => {
-    expect(transformSyntaxToBrowserslist('es6')).toMatchInlineSnapshot(`
+    expect(transformSyntaxToBrowserslist('es2015')).toMatchInlineSnapshot(`
       [
-        "Chrome >= 63.0.0",
-        "Edge >= 79.0.0",
-        "Firefox >= 67.0.0",
-        "iOS >= 13.0.0",
-        "node > 12.20.0 and node < 13.0.0",
-        "node > 13.2.0",
-        "Opera >= 50.0.0",
-        "Safari >= 13.0.0",
+        "chrome >= 63.0.0",
+        "edge >= 79.0.0",
+        "firefox >= 67.0.0",
+        "ios >= 13.0.0",
+        "node >= 13.2.0",
+        "opera >= 50.0.0",
+        "safari >= 13.0.0",
       ]
     `);
 
     expect(transformSyntaxToBrowserslist('es2018')).toMatchInlineSnapshot(`
       [
-        "Chrome >= 64.0.0",
-        "Edge >= 79.0.0",
-        "Firefox >= 78.0.0",
-        "iOS >= 16.4.0",
-        "node > 18.20.0 and node < 19.0.0",
-        "node > 20.12.0 and node < 21.0.0",
-        "node > 21.3.0",
-        "Opera >= 51.0.0",
-        "Safari >= 16.4.0",
+        "chrome >= 64.0.0",
+        "edge >= 79.0.0",
+        "firefox >= 78.0.0",
+        "ios >= 16.4.0",
+        "node >= 13.2.0",
+        "opera >= 51.0.0",
+        "safari >= 16.4.0",
       ]
     `);
 
@@ -84,14 +134,14 @@ describe('transformSyntaxToBrowserslist', () => {
     ).toMatchInlineSnapshot(`
       [
         "Chrome 123",
-        "Chrome >= 5.0.0",
-        "Edge >= 12.0.0",
-        "Firefox >= 2.0.0",
+        "chrome >= 5.0.0",
+        "edge >= 12.0.0",
+        "firefox >= 2.0.0",
         "ie >= 9.0.0",
-        "iOS >= 6.0.0",
-        "Node >= 0.4.0",
-        "Opera >= 10.10.0",
-        "Safari >= 3.1.0",
+        "ios >= 6.0.0",
+        "node >= 0.4.0",
+        "opera >= 10.10.0",
+        "safari >= 3.1.0",
       ]
     `);
 
