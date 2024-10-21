9bbd04bd8234e48e27ef0d14c6b2867e13a684bb
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2024-10-21 16:24:28 +0800

    docs: init website structure (#305)

--- website/theme/index.tsx
+++ website/theme/index.tsx
@@ -1,9 +1,35 @@
+import { Announcement } from '@rstack-dev/doc-ui/announcement';
 import { NavIcon } from '@rstack-dev/doc-ui/nav-icon';
+import { NoSSR, useLang, usePageData } from 'rspress/runtime';
 import Theme from 'rspress/theme';
 import { HomeLayout } from './pages';
 import './index.scss';
 
-const Layout = () => <Theme.Layout beforeNavTitle={<NavIcon />} />;
+const ANNOUNCEMENT_URL = '/';
+
+const Layout = () => {
+  const { page } = usePageData();
+  const lang = useLang();
+  return (
+    <Theme.Layout
+      beforeNavTitle={<NavIcon />}
+      beforeNav={
+        <NoSSR>
+          <Announcement
+            href={ANNOUNCEMENT_URL}
+            message={
+              lang === 'en'
+                ? '🚧 Rslib documentation is under construction, stay tuned for a stable version!'
+                : '🚧 Rslib 中文文档正在施工中，可以先查阅英文文档'
+            }
+            localStorageKey="rslib-announcement-closed"
+            display={page.pageType === 'home'}
+          />
+        </NoSSR>
+      }
+    />
+  );
+};
 
 export default {
   ...Theme,
