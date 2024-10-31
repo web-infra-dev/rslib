92ca3e30e433531e1bd3e3291ee6b865667715ef
nyqykk <nieyan.kk@bytedance.com>
2024-10-31 16:14:30 +0800

    test: fix type check error in ci

--- examples/module-federation/mf-react-component/stories/index.stories.tsx
+++ examples/module-federation/mf-react-component/stories/index.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// @ts-ignore ignore remote module type check for passing ci run build because we set @mf-types folder in .gitignore
 import { Counter } from 'rslib-module';
 
 const Component = () => <Counter />;
