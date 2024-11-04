f9d10413b83a0219139a29293187164d7cd45b18
nyqykk <nieyan.kk@bytedance.com>
2024-11-04 11:44:15 +0800

    chore: re export composeCreateRsbuildConfig in rslib core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 export { prepareCli } from './cli/prepare';
 export { runCli } from './cli/commands';
-export { defineConfig, loadConfig } from './config';
+export { defineConfig, loadConfig, composeCreateRsbuildConfig } from './config';
 export { build } from './build';
 export { logger } from './utils/logger';
 export type * from './types';
