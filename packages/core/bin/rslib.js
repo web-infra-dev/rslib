8d2afe328e754d61a8819bd226f260ec7d07a8ff
neverland <chenjiahan.jait@bytedance.com>
2024-11-04 16:02:21 +0800

    perf(CLI): enable Node cache before loading modules

--- packages/core/bin/rslib.js
+++ packages/core/bin/rslib.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
-import { logger, prepareCli, runCli } from '../dist/index.js';
+import nodeModule from 'node:module';
+
+// enable on-disk code caching of all modules loaded by Node.js
+// requires Nodejs >= 22.8.0
+const { enableCompileCache } = nodeModule;
+if (enableCompileCache && !process.env.NODE_DISABLE_COMPILE_CACHE) {
+  try {
+    enableCompileCache();
+  } catch {
+    // ignore errors
+  }
+}
 
 async function main() {
+  const { logger, prepareCli, runCli } = await import('../dist/index.js');
   prepareCli();
 
   try {
