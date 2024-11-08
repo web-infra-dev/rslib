40d912594243a4dcbe9a0d63fd3f6e33874f6d2d
fi3ework <fi3ework@gmail.com>
2024-11-08 23:24:59 +0800

    feat(command): support --lib arguments

---
+++ tests/integration/cli/inspect.test.ts
@@ -0,0 +1,95 @@
+import { execSync } from 'node:child_process';
+import path from 'node:path';
+import { describe } from 'node:test';
+import fse from 'fs-extra';
+import { globContentJSON } from 'test-helper';
+import { expect, test } from 'vitest';
+
+describe('inspect command', async () => {
+  test('basic', async () => {
+    await fse.remove(path.join(__dirname, 'dist'));
+    execSync('npx rslib inspect', {
+      cwd: __dirname,
+    });
+
+    const files = await globContentJSON(path.join(__dirname, 'dist/.rsbuild'));
+    const fileNames = Object.keys(files).sort();
+
+    expect(fileNames).toMatchInlineSnapshot(`
+    [
+      "<ROOT>/tests/integration/cli/dist/.rsbuild/rsbuild.config.cjs.mjs",
+      "<ROOT>/tests/integration/cli/dist/.rsbuild/rsbuild.config.esm.mjs",
+      "<ROOT>/tests/integration/cli/dist/.rsbuild/rspack.config.cjs.mjs",
+      "<ROOT>/tests/integration/cli/dist/.rsbuild/rspack.config.esm.mjs",
+    ]
+  `);
+
+    // esm rsbuild config
+    const rsbuildConfigEsm = fileNames.find((item) =>
+      item.includes('rsbuild.config.esm.mjs'),
+    );
+    expect(rsbuildConfigEsm).toBeTruthy();
+    expect(files[rsbuildConfigEsm!]).toContain("type: 'modern-module'");
+
+    // esm rspack config
+    const rspackConfigEsm = fileNames.find((item) =>
+      item.includes('rspack.config.esm.mjs'),
+    );
+    expect(rspackConfigEsm).toBeTruthy();
+    expect(files[rspackConfigEsm!]).toContain("type: 'modern-module'");
+  });
+
+  test('--lib', async () => {
+    await fse.remove(path.join(__dirname, 'dist'));
+    execSync('npx rslib inspect --lib esm', {
+      cwd: __dirname,
+    });
+
+    const files = await globContentJSON(
+      path.join(__dirname, 'dist/esm/.rsbuild'),
+    );
+    const fileNames = Object.keys(files).sort();
+
+    // Rsbuild will emit dump files to `dist/esm` if only one environment is specified.
+    expect(fileNames).toMatchInlineSnapshot(`
+    [
+      "<ROOT>/tests/integration/cli/dist/esm/.rsbuild/rsbuild.config.mjs",
+      "<ROOT>/tests/integration/cli/dist/esm/.rsbuild/rspack.config.esm.mjs",
+    ]
+  `);
+
+    // esm rsbuild config
+    const rsbuildConfigEsm = fileNames.find((item) =>
+      item.includes('rsbuild.config.mjs'),
+    );
+    expect(rsbuildConfigEsm).toBeTruthy();
+    expect(files[rsbuildConfigEsm!]).toContain("type: 'modern-module'");
+
+    // esm rspack config
+    const rspackConfigEsm = fileNames.find((item) =>
+      item.includes('rspack.config.esm.mjs'),
+    );
+    expect(rspackConfigEsm).toBeTruthy();
+    expect(files[rspackConfigEsm!]).toContain("type: 'modern-module'");
+  });
+
+  test('--lib multiple', async () => {
+    await fse.remove(path.join(__dirname, 'dist'));
+    execSync('npx rslib inspect --lib esm --lib cjs', {
+      cwd: __dirname,
+    });
+
+    const files = await globContentJSON(path.join(__dirname, 'dist/.rsbuild'));
+    const fileNames = Object.keys(files).sort();
+
+    // Rsbuild will emit dump files to `dist/esm` if only one environment is specified.
+    expect(fileNames).toMatchInlineSnapshot(`
+    [
+      "<ROOT>/tests/integration/cli/dist/.rsbuild/rsbuild.config.cjs.mjs",
+      "<ROOT>/tests/integration/cli/dist/.rsbuild/rsbuild.config.esm.mjs",
+      "<ROOT>/tests/integration/cli/dist/.rsbuild/rspack.config.cjs.mjs",
+      "<ROOT>/tests/integration/cli/dist/.rsbuild/rspack.config.esm.mjs",
+    ]
+  `);
+  });
+});
