e7911f05174325f893142fc2f0608d45655ef536
Timeless0911 <1604889533@qq.com>
2024-11-11 20:20:12 +0800

    chore: bump Rslib 0.0.17

--- packages/create-rslib/rslib.config.ts
+++ packages/create-rslib/rslib.config.ts
@@ -2,8 +2,4 @@ import { defineConfig } from 'rslib';
 
 export default defineConfig({
   lib: [{ format: 'esm' }],
-  // TODO: Remove this after bumping Rslib
-  output: {
-    target: 'node',
-  },
 });
