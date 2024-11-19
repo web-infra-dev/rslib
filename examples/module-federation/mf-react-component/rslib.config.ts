5732401425c92bb64ed017f0f0f2f05caa8e0eda
Wei <fi3ework@gmail.com>
2024-11-19 14:45:33 +0800

    chore: add Preact example (#451)

--- examples/module-federation/mf-react-component/rslib.config.ts
+++ examples/module-federation/mf-react-component/rslib.config.ts
@@ -2,17 +2,11 @@ import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { defineConfig } from '@rslib/core';
 
-const shared = {
-  dts: {
-    bundle: false,
-  },
-};
-
 export default defineConfig({
   lib: [
     {
-      ...shared,
       format: 'esm',
+      dts: true,
       output: {
         distPath: {
           root: './dist/esm',
@@ -20,8 +14,8 @@ export default defineConfig({
       },
     },
     {
-      ...shared,
       format: 'cjs',
+      dts: true,
       output: {
         distPath: {
           root: './dist/cjs',
@@ -29,8 +23,8 @@ export default defineConfig({
       },
     },
     {
-      ...shared,
       format: 'mf',
+      dts: true,
       output: {
         distPath: {
           root: './dist/mf',
