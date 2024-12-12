aa4c5f32848cff3752bab9cf5f43ff38f608acdb
Soon <sooniter@gmail.com>
2024-12-12 18:53:17 +0800

    docs(zh): translate other zh docs (#551)
    Co-authored-by: Timeless0911 <50201324+Timeless0911@users.noreply.github.com>

--- website/theme/index.tsx
+++ website/theme/index.tsx
@@ -1,28 +1,10 @@
-import { Announcement } from '@rstack-dev/doc-ui/announcement';
 import { NavIcon } from '@rstack-dev/doc-ui/nav-icon';
-import { NoSSR, useLang, usePageData } from 'rspress/runtime';
 import Theme from 'rspress/theme';
 import { HomeLayout } from './pages';
 import './index.scss';
 
 const Layout = () => {
-  const { page } = usePageData();
-  const lang = useLang();
-  return (
-    <Theme.Layout
-      beforeNavTitle={<NavIcon />}
-      beforeNav={
-        <NoSSR>
-          <Announcement
-            href={`${page.routePath.replace('/zh', '')}`}
-            message={'🚧 Rslib 中文文档正在施工中，点击查阅英文文档'}
-            localStorageKey="rslib-announcement-closed"
-            display={lang === 'zh'}
-          />
-        </NoSSR>
-      }
-    />
-  );
+  return <Theme.Layout beforeNavTitle={<NavIcon />} />;
 };
 
 export default {
