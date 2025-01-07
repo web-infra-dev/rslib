9faab6d68c87c751147a1379ea66f8b76de955fe
fi3ework <fi3ework@gmail.com>
2025-01-07 15:23:36 +0800

    up

--- tests/integration/externals/user-externals/rslib.config.ts
+++ tests/integration/externals/user-externals/rslib.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from '@rslib/core';
 import { generateBundleEsmConfig } from 'test-helper';
 
+const baseExternals = {
+  externals: { 'lodash/zip': 'lodash-es/zip', 'node:fs': 'memfs' },
+};
+
 export default defineConfig({
-  lib: [generateBundleEsmConfig()],
-  output: {
-    externals: { 'lodash/zip': 'lodash-es/zip', 'node:fs': 'memfs' },
-  },
+  lib: [
+    generateBundleEsmConfig({
+      output: {
+        externals: baseExternals,
+        distPath: {
+          root: 'dist/bundle',
+        },
+      },
+    }),
+    generateBundleEsmConfig({
+      bundle: false,
+      output: {
+        externals: { ...baseExternals, './foo': './foo2' },
+        distPath: {
+          root: 'dist/bundle-false',
+        },
+      },
+    }),
+  ],
 });
