f409e4d48077f3af6f7cf89bbe6870f72c70fd75
程序员包子 <103836393+coderbaozi@users.noreply.github.com>
2025-01-20 19:08:18 +0800

    fix: emit css source map in bundleless mode (#698)

--- tests/integration/sourcemap/index.test.ts
+++ tests/integration/sourcemap/index.test.ts
@@ -41,3 +41,19 @@ test('should generate js inline sourcemap: inline-cheap-module-source-map', asyn
 
   expect(code[0]).toContain('//# sourceMappingURL=data:application/json');
 });
+
+test('should generate css sourcemap file', async () => {
+  const fixturePath = join(__dirname, 'css');
+  const { contents } = await buildAndGetResults({ fixturePath, type: 'css' });
+  const files = Object.keys(contents.esm);
+  const code = Object.values(contents.esm);
+
+  expect(files).toMatchInlineSnapshot(`
+    [
+      "<ROOT>/tests/integration/sourcemap/css/dist/esm/index.css",
+      "<ROOT>/tests/integration/sourcemap/css/dist/esm/index.css.map",
+    ]
+  `);
+
+  expect(code[0]).toContain('/*# sourceMappingURL=index.css.map */');
+});
