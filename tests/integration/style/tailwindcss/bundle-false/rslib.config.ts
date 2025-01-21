a4bd75279673fd1222613a97ce7b2dbd47ede1a3
SoonIter <sooniter@gmail.com>
2025-01-21 14:26:01 +0800

    test: tailwindcss bundle false test case

---
+++ tests/integration/style/tailwindcss/bundle-false/rslib.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@rslib/core';
+import { generateBundleCjsConfig, generateBundleEsmConfig } from 'test-helper';
+
+export default defineConfig({
+  lib: [
+    generateBundleEsmConfig({
+      bundle: false,
+    }),
+    generateBundleCjsConfig({
+      bundle: false,
+    }),
+  ],
+  tools: {
+    lightningcssLoader: false,
+  },
+  output: {
+    target: 'web',
+  },
+});
--- tests/integration/style/tailwindcss/index.test.ts
+++ tests/integration/style/tailwindcss/index.test.ts
@@ -30,3 +30,31 @@ test('should extract css when using tailwindcss successfully in bundle', async (
     '.underline {',
   ]);
 });
+
+test('should extract css when using tailwindcss successfully in bundleless', async () => {
+  const fixturePath = join(__dirname, 'bundle-false');
+  const { contents } = await buildAndGetResults({ fixturePath, type: 'css' });
+  const esmFiles = Object.keys(contents.esm);
+  expect(esmFiles).toMatchInlineSnapshot(`
+    [
+      "<ROOT>/tests/integration/style/tailwindcss/bundle-false/dist/esm/index.css",
+    ]
+  `);
+  expectFileContainContent(contents.esm, 'index.css', [
+    '.text-3xl {',
+    '.font-bold {',
+    '.underline {',
+  ]);
+
+  const cjsFiles = Object.keys(contents.cjs);
+  expect(cjsFiles).toMatchInlineSnapshot(`
+    [
+      "<ROOT>/tests/integration/style/tailwindcss/bundle-false/dist/cjs/index.css",
+    ]
+  `);
+  expectFileContainContent(contents.cjs, 'index.css', [
+    '.text-3xl {',
+    '.font-bold {',
+    '.underline {',
+  ]);
+});
