f33db74e9eec5a5cfe006ab4a28213d63c6ae8c0
fi3ework <fi3ework@gmail.com>
2025-02-07 16:20:35 +0800

    fix(dts): correct tsconfig path not found error message

---
+++ tests/integration/dts/bundle-false/tsconfig-path/rslib.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@rslib/core';
+import { generateBundleEsmConfig } from 'test-helper';
+
+export default defineConfig({
+  lib: [
+    generateBundleEsmConfig({
+      bundle: false,
+      dts: {
+        bundle: false,
+      },
+    }),
+  ],
+  source: {
+    entry: {
+      index: '../__fixtures__/src/index.ts',
+    },
+    tsconfigPath: '../path_not_exist/tsconfig.json',
+  },
+});
