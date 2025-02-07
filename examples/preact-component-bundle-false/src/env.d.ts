5dafcdc129c5ccf3d653023753f8c293d11dfd7c
Soon <sooniter@gmail.com>
2025-02-07 16:50:06 +0800

    feat(core): reexport "@rslib/core/types" from "@rsbuild/core/types" (#739)

--- examples/preact-component-bundle-false/src/env.d.ts
+++ examples/preact-component-bundle-false/src/env.d.ts
@@ -1,9 +1,1 @@
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.svg' {
-  const url: string;
-  export default url;
-}
+/// <reference types="@rslib/core/types" />
--- examples/react-component-umd/src/env.d.ts
+++ examples/react-component-umd/src/env.d.ts
@@ -1,4 +1,1 @@
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
+/// <reference types="@rslib/core/types" />
