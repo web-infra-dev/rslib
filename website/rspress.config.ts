26babe94fd945b81a5df5d1219df5fb9d02352a6
SoonIter <sooniter@gmail.com>
2025-04-25 14:50:58 +0800

    docs(llms.txt): add more description

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
   lang: 'en',
   base: '/',
   title: 'Rslib',
-  description: 'Rsbuild-based library development tool',
+  description:
+    'Rslib is a library development tool that leverages the well-designed configurations and plugins of Rsbuild, empowering library developers to take advantage of the extensive knowledge and ecosystem of webpack and Rspack.',
   icon: 'https://assets.rspack.dev/rslib/rslib-logo-192x192.png',
   logo: 'https://assets.rspack.dev/rslib/rslib-logo-192x192.png',
   logoText: 'Rslib',
