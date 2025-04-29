155a0d4b1a7437b03a6eba453f75f35103d775fa
fi3ework <fi3ework@gmail.com>
2025-04-29 18:59:10 +0800

    feat(template): add Vue

---
+++ packages/create-rslib/template-[vue]-[storybook]-ts/.storybook/main.ts
@@ -0,0 +1,38 @@
+import { dirname, join } from 'node:path';
+import type { StorybookConfig } from 'storybook-vue3-rsbuild';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: getAbsolutePath('storybook-addon-rslib'),
+    },
+  ],
+  framework: {
+    name: getAbsolutePath('storybook-vue3-rsbuild'),
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  typescript: {
+    check: true,
+  },
+};
+
+export default config;
