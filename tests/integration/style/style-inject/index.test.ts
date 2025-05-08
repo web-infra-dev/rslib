6ea2aa6148be6a40dab2cfb97ec2680db41b4482
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2025-05-08 15:06:42 +0800

    docs: output.injectStyles (#969)

--- tests/integration/style/style-inject/index.test.ts
+++ tests/integration/style/style-inject/index.test.ts
@@ -2,7 +2,7 @@ import { join } from 'node:path';
 import { buildAndGetResults } from 'test-helper';
 import { expect, test } from 'vitest';
 
-test('should extract css successfully with `output.styleInject = true` in bundle', async () => {
+test('should extract css successfully with `output.injectStyles = true` in bundle', async () => {
   const fixturePath = join(__dirname, 'bundle');
   const { contents } = await buildAndGetResults({ fixturePath, type: 'css' });
   const esmFiles = Object.keys(contents.esm ?? {});
