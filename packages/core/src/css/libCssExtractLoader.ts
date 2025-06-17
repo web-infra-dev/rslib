f7b979d44a5546080910bf2434cb5fa5738e4a4d
Timeless0911 <1604889533@qq.com>
2025-06-17 20:04:55 +0800

    chore: update

--- packages/core/src/css/libCssExtractLoader.ts
+++ packages/core/src/css/libCssExtractLoader.ts
@@ -7,15 +7,14 @@
  */
 import path, { extname } from 'node:path';
 import type { Rspack } from '@rsbuild/core';
+import {
+  ABSOLUTE_PUBLIC_PATH,
+  AUTO_PUBLIC_PATH,
+  BASE_URI,
+  SINGLE_DOT_PATH_SEGMENT,
+} from './const';
+
 import { type CssLoaderOptionsAuto, isCssModulesFile } from './utils';
-
-export const BASE_URI = 'webpack://';
-export const MODULE_TYPE = 'css/mini-extract';
-export const AUTO_PUBLIC_PATH = '__mini_css_extract_plugin_public_path_auto__';
-export const ABSOLUTE_PUBLIC_PATH: string = `${BASE_URI}/mini-css-extract-plugin/`;
-export const SINGLE_DOT_PATH_SEGMENT =
-  '__mini_css_extract_plugin_single_dot_path_segment__';
-
 interface DependencyDescription {
   identifier: string;
   content: string;
