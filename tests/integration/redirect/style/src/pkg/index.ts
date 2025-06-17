b83d8f9f756549452563bc9c30b6f50d3136ce7d
Wei <fi3ework@gmail.com>
2025-06-17 17:04:39 +0800

    Update tests/integration/redirect/style/src/pkg/index.ts

--- tests/integration/redirect/style/src/pkg/index.ts
+++ tests/integration/redirect/style/src/pkg/index.ts
@@ -1,5 +1,5 @@
 // https://github.com/web-infra-dev/rslib/issues/617
 import 'element-ui/lib/theme-chunk/index.css';
 import 'element-ui/lib/theme-chunk/index';
-// can be resolved, 3rd packages
+// can be resolved, 3rd party packages
 import 'third-party/index.module.scss';
