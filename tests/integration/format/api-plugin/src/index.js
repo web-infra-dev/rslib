9a57592e18fc4ee53d21cdae58df74795fe6b2a3
fi3ework <fi3ework@gmail.com>
2025-08-22 14:29:01 +0800

    test: add more APIPlugin interception

--- tests/integration/format/api-plugin/src/index.js
+++ tests/integration/format/api-plugin/src/index.js
@@ -1,1 +1,6 @@
-export const c = require.cache;
+export const a = require.cache;
+export const b = require.extensions;
+export const c = require.config;
+export const d = require.version;
+export const e = require.include;
+export const f = require.onError;
--- tests/integration/format/index.test.ts
+++ tests/integration/format/index.test.ts
@@ -1,14 +1,14 @@
-import path from 'node:path';
-import { expect, test } from '@rstest/core';
-import { buildAndGetResults } from 'test-helper';
+import path from "node:path";
+import { expect, test } from "@rstest/core";
+import { buildAndGetResults } from "test-helper";
 
-test('format default to esm', async () => {
-  const fixturePath = path.resolve(__dirname, 'default');
-  const { files, contents } = await buildAndGetResults({
-    fixturePath,
-  });
+test("format default to esm", async () => {
+	const fixturePath = path.resolve(__dirname, "default");
+	const { files, contents } = await buildAndGetResults({
+		fixturePath,
+	});
 
-  expect(files).toMatchInlineSnapshot(`
+	expect(files).toMatchInlineSnapshot(`
     {
       "cjs0": [
         "<ROOT>/tests/integration/format/default/dist/bundle-cjs/index.cjs",
@@ -27,7 +27,7 @@ test('format default to esm', async () => {
     }
   `);
 
-  expect(contents.esm0).toMatchInlineSnapshot(`
+	expect(contents.esm0).toMatchInlineSnapshot(`
     {
       "<ROOT>/tests/integration/format/default/dist/bundle-esm/index.js": "const foo = 'foo';
     const str = 'hello' + foo + ' world';
@@ -36,7 +36,7 @@ test('format default to esm', async () => {
     }
   `);
 
-  expect(contents.esm1).toMatchInlineSnapshot(`
+	expect(contents.esm1).toMatchInlineSnapshot(`
     {
       "<ROOT>/tests/integration/format/default/dist/bundleless-esm/foo.js": "const foo = 'foo';
     export { foo };
@@ -49,19 +49,19 @@ test('format default to esm', async () => {
   `);
 });
 
-test('import.meta.url should be preserved', async () => {
-  const fixturePath = path.resolve(__dirname, 'import-meta-url');
-  const { files, entries, entryFiles } = await buildAndGetResults({
-    fixturePath,
-  });
-  expect(files).toMatchInlineSnapshot(`
+test("import.meta.url should be preserved", async () => {
+	const fixturePath = path.resolve(__dirname, "import-meta-url");
+	const { files, entries, entryFiles } = await buildAndGetResults({
+		fixturePath,
+	});
+	expect(files).toMatchInlineSnapshot(`
     {
       "esm": [
         "<ROOT>/tests/integration/format/import-meta-url/dist/esm/index.js",
       ],
     }
   `);
-  expect(entries.esm).toMatchInlineSnapshot(`
+	expect(entries.esm).toMatchInlineSnapshot(`
     "import node_url from "node:url";
     const packageDirectory = node_url.fileURLToPath(new URL('.', import.meta.url));
     const foo = 'foo';
@@ -69,8 +69,8 @@ test('import.meta.url should be preserved', async () => {
     "
   `);
 
-  const result = await import(entryFiles.esm);
-  expect(result).toMatchInlineSnapshot(`
+	const result = await import(entryFiles.esm);
+	expect(result).toMatchInlineSnapshot(`
     {
       "foo": "foo",
       "packageDirectory": "<ROOT>/tests/integration/format/import-meta-url/dist/esm/",
@@ -78,18 +78,18 @@ test('import.meta.url should be preserved', async () => {
   `);
 });
 
-test('CJS exports should be statically analyzable (cjs-module-lexer for Node.js)', async () => {
-  const fixturePath = path.resolve(__dirname, 'cjs-static-export');
-  const { entryFiles } = await buildAndGetResults({
-    fixturePath,
-  });
+test("CJS exports should be statically analyzable (cjs-module-lexer for Node.js)", async () => {
+	const fixturePath = path.resolve(__dirname, "cjs-static-export");
+	const { entryFiles } = await buildAndGetResults({
+		fixturePath,
+	});
 
-  const { bar, foo } = await import(entryFiles.cjs);
-  expect(bar).toBe('bar');
-  expect(foo).toBe('foo');
+	const { bar, foo } = await import(entryFiles.cjs);
+	expect(bar).toBe("bar");
+	expect(foo).toBe("foo");
 
-  const cjs = await import(entryFiles.cjs);
-  expect(cjs).toMatchInlineSnapshot(`
+	const cjs = await import(entryFiles.cjs);
+	expect(cjs).toMatchInlineSnapshot(`
     {
       "bar": "bar",
       "default": {
@@ -101,28 +101,38 @@ test('CJS exports should be statically analyzable (cjs-module-lexer for Node.js)
   `);
 });
 
-test('throw error when using mf with `bundle: false`', async () => {
-  const fixturePath = path.resolve(__dirname, 'mf-bundle-false');
-  const build = buildAndGetResults({
-    fixturePath,
-  });
+test("throw error when using mf with `bundle: false`", async () => {
+	const fixturePath = path.resolve(__dirname, "mf-bundle-false");
+	const build = buildAndGetResults({
+		fixturePath,
+	});
 
-  await expect(build).rejects.toThrowErrorMatchingInlineSnapshot(
-    `[Error: When using "mf" format, "bundle" must be set to "true". Since the default value for "bundle" is "true", so you can either explicitly set it to "true" or remove the field entirely.]`,
-  );
+	await expect(build).rejects.toThrowErrorMatchingInlineSnapshot(
+		`[Error: When using "mf" format, "bundle" must be set to "true". Since the default value for "bundle" is "true", so you can either explicitly set it to "true" or remove the field entirely.]`,
+	);
 });
 
 test("API plugin's api should be skipped in parser", async () => {
-  const fixturePath = path.resolve(__dirname, 'api-plugin');
-  const { entries } = await buildAndGetResults({
-    fixturePath,
-  });
+	const fixturePath = path.resolve(__dirname, "api-plugin");
+	const { entries } = await buildAndGetResults({
+		fixturePath,
+	});
 
-  expect(entries.esm).toMatchInlineSnapshot(`
-    "const c = require.cache;
-    export { c };
-    "
-  `);
+	expect(entries.esm).toMatchInlineSnapshot(`
+		"const a = require.cache;
+		const b = require.extensions;
+		const c = require.config;
+		const d = require.version;
+		const e = require.include;
+		const f = require.onError;
+		export { a, b, c, d, e, f };
+		"
+	`);
 
-  expect(entries.cjs).toContain('const c = require.cache;');
+	expect(entries.cjs).toContain("const a = require.cache;");
+	expect(entries.cjs).toContain("const b = require.extensions;");
+	expect(entries.cjs).toContain("const c = require.config;");
+	expect(entries.cjs).toContain("const d = require.version;");
+	expect(entries.cjs).toContain("const e = require.include;");
+	expect(entries.cjs).toContain("const f = require.onError;");
 });
