3c6aa044c0cb916712c3901bbb00b2176158f6bd
Wei <fi3ework@gmail.com>
2025-08-26 19:59:58 +0800

    test: add more APIPlugin interception (#1184)
    Co-authored-by: Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
    Co-authored-by: Timeless0911 <1604889533@qq.com>

--- tests/integration/format/index.test.ts
+++ tests/integration/format/index.test.ts
@@ -119,10 +119,20 @@ test("API plugin's api should be skipped in parser", async () => {
   });
 
   expect(entries.esm).toMatchInlineSnapshot(`
-    "const c = require.cache;
-    export { c };
-    "
-  `);
+		"const a = require.cache;
+		const b = require.extensions;
+		const c = require.config;
+		const d = require.version;
+		const e = require.include;
+		const f = require.onError;
+		export { a, b, c, d, e, f };
+		"
+	`);
 
-  expect(entries.cjs).toContain('const c = require.cache;');
+  expect(entries.cjs).toContain('const a = require.cache;');
+  expect(entries.cjs).toContain('const b = require.extensions;');
+  expect(entries.cjs).toContain('const c = require.config;');
+  expect(entries.cjs).toContain('const d = require.version;');
+  expect(entries.cjs).toContain('const e = require.include;');
+  expect(entries.cjs).toContain('const f = require.onError;');
 });
