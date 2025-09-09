9c5447ebea92ae62271b2cd015ddfdafdba4d59d
Timeless0911 <1604889533@qq.com>
2025-09-09 11:12:11 +0800

    chore: update

--- packages/core/rslib.config.ts
+++ packages/core/rslib.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
       syntax: ['node 18.12.0'],
       dts: {
         bundle: false,
+        // Only use tsgo in local dev for faster build, disable it in CI until it's more stable
         tsgo: !process.env.CI,
         distPath: './dist-types',
       },
