cc23ec93e2a16e6b5967b14aacef875078f64315
Timeless0911 <1604889533@qq.com>
2025-09-16 19:16:13 +0800

    chore: update

--- tests/scripts/helper.ts
+++ tests/scripts/helper.ts
@@ -72,6 +72,7 @@ export const expectPoll: (fn: () => boolean) => ReturnType<typeof expect.poll> =
   (fn) => {
     return expect.poll(fn, {
       intervals: [20, 30, 40, 50, 60, 70, 80, 90, 100],
+      timeout: process.env.CI ? 10_000 : 5_000,
     });
   };
 
