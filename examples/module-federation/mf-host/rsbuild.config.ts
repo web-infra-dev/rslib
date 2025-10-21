29f41ac22069d043ad6aa1bc7f9044567122d232
Timeless0911 <1604889533@qq.com>
2025-10-21 11:49:29 +0800

    chore: disbale lazyCompilation to make mf e2e case stable

--- examples/module-federation/mf-host/rsbuild.config.ts
+++ examples/module-federation/mf-host/rsbuild.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
+  dev: {
+    lazyCompilation: false,
+  },
   plugins: [
     pluginReact(),
     pluginModuleFederation(
