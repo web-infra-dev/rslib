3ee894b1d772546ec1db22fe6a10bf2338c94169
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2025-11-17 16:39:52 +0800

    docs: transparent nav at top (#1341)

--- website/theme/pages/index.tsx
+++ website/theme/pages/index.tsx
@@ -1,12 +1,23 @@
+import { HomeBackground as BasicHomeBackground } from '@rspress/core/theme';
 import { BackgroundImage } from '@rstack-dev/doc-ui/background-image';
 import { CopyRight } from '../components/Copyright';
 import { Hero } from '../components/Hero';
 import { ToolStack } from '../components/ToolStack';
 
+function HomeBackground() {
+  return (
+    <>
+      {/* For transparent nav at top */}
+      <BasicHomeBackground style={{ background: 'none' }} />
+      <BackgroundImage />
+    </>
+  );
+}
+
 export function HomeLayout() {
   return (
     <div style={{ position: 'relative' }}>
-      <BackgroundImage />
+      <HomeBackground />
       <Hero />
       <ToolStack />
       <CopyRight />
