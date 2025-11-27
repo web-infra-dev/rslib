f880479dbe191527cbdf5c8717d2c84e4de112a9
fi3ework <fi3ework@gmail.com>
2025-11-27 15:29:23 +0800

    chore: bump SB

--- packages/create-rslib/template-[react]-[rstest,storybook]-js/.storybook/main.js
+++ packages/create-rslib/template-[react]-[rstest,storybook]-js/.storybook/main.js
@@ -1,15 +1,18 @@
-import { createRequire } from 'node:module';
-import { dirname, join } from 'node:path';
-
-const require = createRequire(import.meta.url);
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
+const getAbsolutePath = (value) => {
+  return resolve(
+    fileURLToPath(
+      new URL(import.meta.resolve(`${value}/package.json`, import.meta.url)),
+    ),
+    '..',
+  );
+};
 
 const config = {
   stories: [
