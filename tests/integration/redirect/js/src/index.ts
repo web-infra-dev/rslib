638dae6c73ba6f2d5ade0050269f5a173a22be4f
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2025-12-01 20:00:32 +0800

    fix: preserve original extension when extension redirection is disabled (#1369)

--- tests/integration/redirect/js/src/index.ts
+++ tests/integration/redirect/js/src/index.ts
@@ -1,6 +1,5 @@
-// can not be resolved
-import lodash from 'lodash';
 // can be resolved, 3rd party packages
+import lodash from 'lodash';
 import merge from 'lodash.merge';
 // can be resolved but not specified -- phantom dependency
 import prettier from 'prettier';
@@ -14,6 +13,11 @@ import { foo } from './foo';
 
 export * from './.hidden';
 export * from './.hidden-folder';
+export * from './bar.node';
+export * from './bar.node.js';
+export * from './bar.node.ts';
+export * from './foo.js';
+export * from './foo.ts';
 
 export default lodash.toUpper(
   merge(foo) + bar + foo2 + bar2 + baz + typeof prettier.version,
