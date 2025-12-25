1ebb3ec0d71454e8ca3fc9510bab13a7ef07ece8
9aoy <9aoyuao@gmail.com>
2025-12-25 19:12:28 +0800

    feat(create-rslib): use happy-dom instead of jsdom as default testEnvironment in rstest template

--- packages/create-rslib/fragments/tools/rstest-react-js/rstest.config.js
+++ packages/create-rslib/fragments/tools/rstest-react-js/rstest.config.js
@@ -2,7 +2,7 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import { defineConfig } from '@rstest/core';
 
 export default defineConfig({
-  testEnvironment: 'jsdom',
+  testEnvironment: 'happy-dom',
   setupFiles: ['./rstest.setup.js'],
   plugins: [pluginReact()],
 });
--- packages/create-rslib/template-[vue]-[rstest,storybook]-ts/rstest.config.ts
+++ packages/create-rslib/template-[vue]-[rstest,storybook]-ts/rstest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@rstest/core';
 import { pluginUnpluginVue } from 'rsbuild-plugin-unplugin-vue';
 
 export default defineConfig({
-  testEnvironment: 'jsdom',
+  testEnvironment: 'happy-dom',
   setupFiles: ['./rstest.setup.ts'],
   plugins: [pluginUnpluginVue()],
 });
