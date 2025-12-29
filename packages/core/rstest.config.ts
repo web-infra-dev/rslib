8440907144886d786dc317aec81b9f083cf59bc2
Timeless0911 <50201324+Timeless0911@users.noreply.github.com>
2025-12-29 16:18:40 +0800

    chore(deps): bump Rslib v0.19.0 (#1417)

--- packages/core/rstest.config.ts
+++ packages/core/rstest.config.ts
@@ -1,8 +1,12 @@
+import { withRslibConfig } from '@rstest/adapter-rslib';
 import { defineConfig } from '@rstest/core';
 import { shared } from '../../rstest.config';
 
 export default defineConfig({
   ...shared,
+  extends: withRslibConfig({
+    cwd: import.meta.dirname,
+  }),
   name: 'unit',
   setupFiles: ['./setupRstestTests.ts'],
 });
--- packages/create-rslib/rslib.config.ts
+++ packages/create-rslib/rslib.config.ts
@@ -8,9 +8,6 @@ export default defineConfig({
     {
       format: 'esm',
       syntax: ['node 18.12.0'],
-      experiments: {
-        advancedEsm: true,
-      },
     },
   ],
   plugins: [
