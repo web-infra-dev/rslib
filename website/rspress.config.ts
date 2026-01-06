86768bcc2b21f2b456dab9e4f211f5dd5bdc9498
Timeless0911 <1604889533@qq.com>
2026-01-06 17:24:36 +0800

    feat(website): enable ssg-md to support rendering overview component

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -2,7 +2,6 @@ import path from 'node:path';
 import { pluginSass } from '@rsbuild/plugin-sass';
 import { defineConfig } from '@rspress/core';
 import { pluginAlgolia } from '@rspress/plugin-algolia';
-import { pluginLlms } from '@rspress/plugin-llms';
 import { pluginRss } from '@rspress/plugin-rss';
 import { pluginSitemap } from '@rspress/plugin-sitemap';
 import { pluginTwoslash } from '@rspress/plugin-twoslash';
@@ -35,10 +34,10 @@ export default defineConfig({
   search: {
     codeBlocks: true,
   },
+  llms: true,
   plugins: [
     pluginAlgolia(),
     pluginFontOpenSans(),
-    pluginLlms(),
     pluginTwoslash({
       cache: false,
     }),
