2c6162579e7a23ad0b623de5615c0d1e945cf3ad
Timeless0911 <daichenwei@bytedance.com>
2026-06-17 17:45:08 +0800

    fix(core): preserve query in bundleless redirects

--- tests/integration/redirect/style/src/module/index.ts
+++ tests/integration/redirect/style/src/module/index.ts
@@ -1,4 +1,7 @@
 // @ts-ignore env.d.ts
 import styles from '@/module/index.module.less';
+// @ts-ignore env.d.ts
+import inlineStyles from '@/module/index.module.less?inline';
 
 console.log('styles: ', styles);
+console.log('inline styles: ', inlineStyles);
