09b3038e9815f0861b8a17f1707db6017e8c025d
Elecmonkey <elecmonkey@hotmail.com>
2026-06-30 11:26:23 +0800

    docs(create-rslib): simplify Solid template config examples

--- packages/create-rslib/template-solid-js/rslib.config.js
+++ packages/create-rslib/template-solid-js/rslib.config.js
@@ -3,11 +3,6 @@ import { pluginSolid } from '@rsbuild/plugin-solid';
 import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
-  source: {
-    entry: {
-      index: ['./src/**'],
-    },
-  },
   lib: [
     {
       bundle: false,
