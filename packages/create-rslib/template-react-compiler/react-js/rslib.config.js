a5a7e1b59e04af223a01c2f1cdb68fc42516839f
Elecmonkey <elecmonkey@hotmail.com>
2026-06-30 16:27:51 +0800

    feat(create-rslib): use Rust-powered React Compiler via reactCompiler option (#1729)

--- packages/create-rslib/template-react-compiler/react-js/rslib.config.js
+++ packages/create-rslib/template-react-compiler/react-js/rslib.config.js
@@ -1,4 +1,3 @@
-import { pluginBabel } from '@rsbuild/plugin-babel';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { defineConfig } from '@rslib/core';
 
@@ -18,14 +17,8 @@ export default defineConfig({
     target: 'web',
   },
   plugins: [
-    pluginReact(),
-    pluginBabel({
-      include: /\.[jt]sx?$/,
-      exclude: [/[\\/]node_modules[\\/]/],
-      babelLoaderOptions(opts) {
-        opts.plugins ??= [];
-        opts.plugins.unshift('babel-plugin-react-compiler');
-      },
+    pluginReact({
+      reactCompiler: true,
     }),
   ],
 });
--- packages/create-rslib/test/helper.ts
+++ packages/create-rslib/test/helper.ts
@@ -161,21 +161,11 @@ export const createAndValidate = (
     expect(pkgJson.devDependencies['react-dom']).toBeTruthy();
 
     if (templateCase.tools.includes('react-compiler')) {
-      expect(pkgJson.devDependencies['@rsbuild/plugin-babel']).toBeTruthy();
-      expect(
-        pkgJson.devDependencies['babel-plugin-react-compiler'],
-      ).toBeTruthy();
-      expect(configContent).toContain('pluginBabel');
-      expect(configContent).toContain('babel-plugin-react-compiler');
+      expect(configContent).toContain('reactCompiler');
       expect(pkgJson.peerDependencies.react).toBe('>=19.0.0');
       expect(pkgJson.peerDependencies['react-dom']).toBe('>=19.0.0');
     } else {
-      expect(pkgJson.devDependencies['@rsbuild/plugin-babel']).toBeFalsy();
-      expect(
-        pkgJson.devDependencies['babel-plugin-react-compiler'],
-      ).toBeFalsy();
-      expect(configContent).not.toContain('pluginBabel');
-      expect(configContent).not.toContain('babel-plugin-react-compiler');
+      expect(configContent).not.toContain('reactCompiler');
       expect(pkgJson.peerDependencies.react).toBe('>=16.14.0');
       expect(pkgJson.peerDependencies['react-dom']).toBe('>=16.14.0');
     }
