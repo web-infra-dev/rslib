0ada5c6b4a7b3071f9dcf4a932b19ce7d257e60d
neverland <chenjiahan.jait@bytedance.com>
2026-07-17 17:50:52 +0800

    refactor: remove prebundle setup

--- packages/core/src/utils/color.ts
+++ packages/core/src/utils/color.ts
@@ -1,1 +1,23 @@
-export { color } from 'rslog';
+import { styleText } from 'node:util';
+
+type ColorFn = (text: string | number) => string;
+type ColorMap = Record<
+  'blue' | 'bold' | 'cyan' | 'dim' | 'gray' | 'green' | 'magenta' | 'yellow',
+  ColorFn
+>;
+
+const createStyler =
+  (style: Parameters<typeof styleText>[0]): ColorFn =>
+  (text) =>
+    styleText(style, String(text));
+
+export const color: ColorMap = {
+  blue: createStyler('blue'),
+  bold: createStyler('bold'),
+  cyan: createStyler('cyan'),
+  dim: createStyler('dim'),
+  gray: createStyler('gray'),
+  green: createStyler('green'),
+  magenta: createStyler('magenta'),
+  yellow: createStyler('yellow'),
+};
