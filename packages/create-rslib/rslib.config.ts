e2fe532128c8f7994e9500314ee7b6d5d45e6b5c
Timeless0911 <daichenwei@bytedance.com>
2026-07-24 17:44:22 +0800

    chore(deps): upgrade Rslib to 1.0.0-beta.1

--- packages/create-rslib/rslib.config.ts
+++ packages/create-rslib/rslib.config.ts
@@ -2,11 +2,5 @@ import { pluginPublint } from 'rsbuild-plugin-publint';
 import { defineConfig } from 'rslib';
 
 export default defineConfig({
-  lib: [
-    {
-      format: 'esm',
-      syntax: ['es2023'],
-    },
-  ],
   plugins: [pluginPublint()],
 });
