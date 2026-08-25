fc0d368cfcf24627a4930b18f2daa81437b11a30
Wu Shuwen <mikewushuwen@outlook.com>
2026-08-25 11:40:04 +0800

    docs: fix outdated external links (#1874)

--- packages/create-rslib/template-storybook/react-ts/stories/Button.stories.ts
+++ packages/create-rslib/template-storybook/react-ts/stories/Button.stories.ts
@@ -12,7 +12,7 @@ const meta = {
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  // More on argTypes: https://storybook.js.org/docs/api/arg-types
   argTypes: {
     backgroundColor: { control: 'color' },
   },
