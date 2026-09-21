d086c689951acb8480063c621262b1b9eb62ea79
Elecmonkey <elecmonkey@hotmail.com>
2026-09-21 15:23:50 +0800

    test(wasm): cover inline import types

--- tests/integration/wasm-inline/typed/src/index.ts
+++ tests/integration/wasm-inline/typed/src/index.ts
@@ -1,3 +1,5 @@
-import { add } from './add.wasm?inline';
+import wasm, { add } from './add.wasm?inline';
 
-export const useAdd = (a: number, b: number): number => add(a, b);
+export const useAdd = (a: number, b: number): number => wasm.add(a, b);
+
+export const useNamedAdd = (a: number, b: number): number => add(a, b);
